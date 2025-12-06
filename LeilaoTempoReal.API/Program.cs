38ad0e851a2757d1d61fffaf0c62d9dea6e379d7
leticiatakenaka <leticiatakenaka12@gmail.com>
2025-12-06 17:31:17 -0300

    implementa leitura do leilao

--- LeilaoTempoReal.API/Program.cs
+++ LeilaoTempoReal.API/Program.cs
@@ -1,5 +1,7 @@
 using LeilaoTempoReal.API.Hubs;
+using LeilaoTempoReal.Dominio.Interfaces;
 using LeilaoTempoReal.Infraestrutura.Dados;
+using LeilaoTempoReal.Infraestrutura.Repositorios;
 using Microsoft.EntityFrameworkCore;
 using StackExchange.Redis;
 
@@ -8,20 +10,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<LeilaoDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("LeilaoDb")));
 
+builder.Services.AddScoped<ILeilaoRepository, LeilaoRepository>();
+
 var redisConnection = ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("Redis"));
 
 builder.Services.AddSingleton<IConnectionMultiplexer>(redisConnection);
 builder.Services.AddOpenApi();
 builder.Services.AddSignalR();
-
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
-app.MapGet("/teste-redis", async (IConnectionMultiplexer mux) => {
-    var db = mux.GetDatabase();
-    await db.StringSetAsync("teste", "Funcionou! O Redis está vivo.");
-    var valor = await db.StringGetAsync("teste");
-    return Results.Ok(valor.ToString());
-});
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.MapHub<LeilaoHub>("/hub/leilao");
 
---
+++ LeilaoTempoReal.Infraestrutura/Repositorios/LeilaoRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using LeilaoTempoReal.Dominio.Entidades;
+using LeilaoTempoReal.Dominio.Interfaces;
+using LeilaoTempoReal.Infraestrutura.Dados;
+using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
+
+namespace LeilaoTempoReal.Infraestrutura.Repositorios;
+
+public class LeilaoRepository(LeilaoDbContext context, IConnectionMultiplexer redisConnection) : ILeilaoRepository
+{
+    private readonly LeilaoDbContext _context = context;
+    private readonly IDatabase _redis = redisConnection.GetDatabase();
+
+    public async Task<Leilao?> ObterPorIdAsync(int id)
+    {
+        var chave = $"leilao:{id}";
+
+        var jsonRedis = await _redis.StringGetAsync(chave);
+
+        if (!jsonRedis.IsNullOrEmpty)
+        {
+            return JsonSerializer.Deserialize<Leilao>(jsonRedis.ToString());
+        }
+
+        var leilao = await _context.Leiloes.FirstOrDefaultAsync(l => l.Id == id);
+
+        if (leilao != null)
+        {
+            var jsonParaSalvar = JsonSerializer.Serialize(leilao);
+            await _redis.StringSetAsync(chave, jsonParaSalvar, TimeSpan.FromHours(1));
+        }
+
+        return leilao;
+    }
+}
